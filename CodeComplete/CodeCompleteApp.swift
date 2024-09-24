0b03d8f76a9e474df1c83bc1f19d629b3027774e
mauryalamin <mauryalamin@me.com>
2024-09-24 17:38:00 -0500

    Initial Commit

---
+++ CodeComplete/CodeCompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCompleteApp.swift
+//  CodeComplete
+//
+//  Created by Maury Alamin on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeCompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeCompleteTests/CodeCompleteTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCompleteTests.swift
+//  CodeCompleteTests
+//
+//  Created by Maury Alamin on 9/24/24.
+//
+
+import Testing
+@testable import CodeComplete
+
+struct CodeCompleteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
